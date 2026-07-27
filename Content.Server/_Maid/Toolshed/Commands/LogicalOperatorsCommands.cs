f886bd8c5f3c7f189cb8ce8d88a610561b596c38
CREAsTIVE <62616308+CREAsTIVE@users.noreply.github.com>
2026-07-27 20:47:36 +0500

    fix: logic toolshed commands

--- Content.Server/_Maid/Toolshed/Commands/LogicalOperatorsCommands.cs
+++ Content.Server/_Maid/Toolshed/Commands/LogicalOperatorsCommands.cs
@@ -1,42 +1,22 @@
-using System;
 using Content.Server.Administration;
 using Content.Shared.Administration;
 using Robust.Shared.Toolshed;
-using Robust.Shared.Toolshed.Syntax;
 
 namespace Content.Server._Maid.Toolshed.Commands;
 
 [ToolshedCommand(Name = "&&"), AdminCommand(AdminFlags.VarEdit)]
 public sealed class LogicalAndCommand : ToolshedCommand
 {
-    public static bool ToBool(object? obj)
-    {
-        if (obj is bool b)
-            return b;
-
-        return obj is null;
-    }
-
     [CommandImplementation]
     public bool And([PipedArgument] bool x, bool y) => x && y;
+}
 
+// For "or" consistency
+[ToolshedCommand(Name = "and"), AdminCommand(AdminFlags.VarEdit)]
+public sealed class WordAndCommand : ToolshedCommand
+{
     [CommandImplementation]
-    public bool And(IInvocationContext ctx, [PipedArgument] bool x, Block<bool> y)
-    {
-        return x && (bool)y.Invoke(null, ctx)!;
-    }
-
-    [CommandImplementation]
-    public bool And([PipedArgument] object? x, object? y)
-    {
-        return ToBool(x) && ToBool(y);
-    }
-
-    [CommandImplementation]
-    public bool And(IInvocationContext ctx, [PipedArgument] object? x, Block y)
-    {
-        return ToBool(x) && ToBool(y.Invoke(null, ctx));
-    }
+    public bool And([PipedArgument] bool x, bool y) => x && y;
 }
 
 // We can't do "||" cause of pipe operator
@@ -45,22 +25,4 @@ public sealed class LogicalOrCommand : ToolshedCommand
 {
     [CommandImplementation]
     public bool Or([PipedArgument] bool x, bool y) => x || y;
-
-    [CommandImplementation]
-    public bool Or(IInvocationContext ctx, [PipedArgument] bool x, Block<bool> y)
-    {
-        return x || (bool)y.Invoke(null, ctx)!;
-    }
-
-    [CommandImplementation]
-    public bool Or([PipedArgument] object? x, object? y)
-    {
-        return LogicalAndCommand.ToBool(x) || LogicalAndCommand.ToBool(y);
-    }
-
-    [CommandImplementation]
-    public bool Or(IInvocationContext ctx, [PipedArgument] object? x, Block y)
-    {
-        return LogicalAndCommand.ToBool(x) || LogicalAndCommand.ToBool(y.Invoke(null, ctx));
-    }
 }
