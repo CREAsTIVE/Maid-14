943c75b3f953b7a76d39b131286ff0a83c5b41d9
CREAsTIVE <62616308+CREAsTIVE@users.noreply.github.com>
2026-07-28 22:42:07 +0500

    fix: some more pushes

--- Content.Server/_Maid/Toolshed/Commands/SolutionUtilsCommands.cs
+++ Content.Server/_Maid/Toolshed/Commands/SolutionUtilsCommands.cs
@@ -1,12 +1,16 @@
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using Content.Server.Administration;
 using Content.Shared.Administration;
 using Content.Shared.Chemistry.EntitySystems;
 using Content.Goobstation.Maths.FixedPoint;
 using Robust.Shared.GameObjects;
+using Robust.Shared.Maths;
 using Robust.Shared.Toolshed;
+using Robust.Shared.Toolshed.Errors;
 using Robust.Shared.Toolshed.Syntax;
+using Robust.Shared.Utility;
 
 namespace Content.Server._Maid.Toolshed.Commands;
 
@@ -24,7 +28,24 @@ public sealed class SolutionUtilsCommands : ToolshedCommand
         [CommandArgument] float maxVol
     )
     {
-        SolutionContainer.EnsureSolutionEntity(input, solutionName, out _, FixedPoint2.New(maxVol));
-        return input;
+        if (SolutionContainer.EnsureSolutionEntity(input, solutionName, out var solutionEntity, FixedPoint2.New(maxVol)))
+        {
+            return input;
+        }
+
+        ctx.ReportError(new SolutionCreationFailedError(solutionName, input));
+        return EntityUid.Invalid;
     }
+}
+
+public record struct SolutionCreationFailedError(string SolutionName, EntityUid Entity) : IConError
+{
+    public FormattedMessage DescribeInner()
+    {
+        return FormattedMessage.FromUnformatted($"Failed to ensure solution '{SolutionName}' on entity {Entity}.");
+    }
+
+    public string? Expression { get; set; }
+    public Vector2i? IssueSpan { get; set; }
+    public StackTrace? Trace { get; set; }
 }
